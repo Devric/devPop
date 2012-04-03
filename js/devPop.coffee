235da1dfa111ad187f15671aaa3e3d4fb8300b1b
Devric <billbox13@gmail.com>
2012-04-04 03:07:28 +1000

    first version

--- js/devPop.coffee
+++ js/devPop.coffee
@@ -1,27 +1,94 @@
 $ = jQuery
 
 $(document).ready ->
-  $('.popit').devPop();
+  $('.popit').devPop({debug:true})
 
 $.fn.extend {}=
   devPop: (options) ->
     settings =
-      debug   : false
+      width  : 500
+      height : 600
+      debug  : false
       
     settings = $.extend settings, options
     
     window.log = (msg) ->      # Simplify logger()
       console?.log msg if settings.debug
     
+    # create devPop
+    log '+ creating modal'
+    _createModal(settings.height, settings.width)
+
     # _Insert magic here._
     return @each ()->
 
-      o   = settings
-      obj = $(@)
+      log "** devPop init for: " + $(@).attr('href')
 
-      log "devTab init for: " + obj.attr('id')
+      o         = settings
+      obj       = $(@)
 
-      log "\n ========== END =========== \n "
+      $id       = obj.attr('href')
+      $target   = $($id)
+
+      $pop      = $('#devPop')
+      $popModal = $pop.find('.devPopModal')
+      $popBg    = $pop.find('.devPopBg')
+
+      # keep position with scroll
+      # simulate position:fix for mobile devices
+      $(window).scroll( ->
+        $('#devPop').css
+            top: $(window).scrollTop()
+      )
+
+      log '+ set up dom'
+      _setupDom(obj)
+
+      # show on object click
+      # ==============================
+      obj.on('click', $target, (e)->
+        log '+ click action'
+        e.preventDefault()
+        $target.show()
+               .siblings('div')
+               .hide()
+
+        # animate bg and modal open
+        $popBg.animate
+          opacity:1
+        , 150, ->
+          $popModal.animate
+            top: 50
+          , 500
+
+
+        log ' - show content'
+        $('#devPop').show()
+
+        $('html').addClass('noscroll')
+
+        # find width and position $modal
+          
+      )
+
+      # hide on click
+      # ==============================
+      $('body').on('click', '.devPopBg, .devPopModal span.close', (e)->
+        e.preventDefault()
+
+        $popModal.animate
+          top: -1000
+        , 'fast', ->
+          $popBg.animate
+            opacity: 0
+          , 'slow' , ->
+            $('#devPop').hide()
+
+        $('html').removeClass('noscroll')
+
+      )
+
+      log "\n========== END =========== \n "
 
 
 # ====================================
@@ -29,5 +96,37 @@ $.fn.extend {}=
 # ====================================
 
 
-# initial setup
+# create modal
 # ====================================
+_createModal = () ->
+  # add containers
+  $('body').prepend('<div id="devPop" style="display:none"><div class="devPopModal paperstack"><span class="close">X</span></div><div class="devPopBg"/></div>')
+  
+  log ' - modal created'
+
+  $height = $('.devPopModal').height()
+  log $width = ( $(window).width() - $('.devPopModal').outerWidth() ) / 2
+  log $('.devPopModal').width()
+  log $('.devPopModal').outerWidth()
+  log $(window).width()
+
+  $('.devPopModal').css
+    top: -1 * ($height + 50)
+    left: $width
+
+# build each obj
+# ====================================
+_setupDom = (el) ->
+  $id       = el.attr('href')
+  $target   = $($id)
+  $pop      = $('#devPop')
+  $popModal = $pop.find('.devPopModal')
+
+  log ' - hide content on init'
+  $target.css
+    'display':'none'
+
+  log ' - move content to modal'
+  $target.prependTo($popModal)
+
+  log ' - dom set'
--- js/devPop.js
+++ js/devPop.js
@@ -1,16 +1,20 @@
 (function() {
-  var $;
+  var $, _createModal, _setupDom;
 
   $ = jQuery;
 
   $(document).ready(function() {
-    return $('.popit').devPop();
+    return $('.popit').devPop({
+      debug: true
+    });
   });
 
   $.fn.extend({
     devPop: function(options) {
       var settings;
       settings = {
+        width: 500,
+        height: 600,
         debug: false
       };
       settings = $.extend(settings, options);
@@ -19,14 +23,86 @@
           return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
         }
       };
+      log('+ creating modal');
+      _createModal(settings.height, settings.width);
       return this.each(function() {
-        var o, obj;
+        var $id, $pop, $popBg, $popModal, $target, o, obj;
+        log("** devPop init for: " + $(this).attr('href'));
         o = settings;
         obj = $(this);
-        log("devTab init for: " + obj.attr('id'));
-        return log("\n ========== END =========== \n ");
+        $id = obj.attr('href');
+        $target = $($id);
+        $pop = $('#devPop');
+        $popModal = $pop.find('.devPopModal');
+        $popBg = $pop.find('.devPopBg');
+        $(window).scroll(function() {
+          return $('#devPop').css({
+            top: $(window).scrollTop()
+          });
+        });
+        log('+ set up dom');
+        _setupDom(obj);
+        obj.on('click', $target, function(e) {
+          log('+ click action');
+          e.preventDefault();
+          $target.show().siblings('div').hide();
+          $popBg.animate({
+            opacity: 1
+          }, 150, function() {
+            return $popModal.animate({
+              top: 50
+            }, 500);
+          });
+          log(' - show content');
+          $('#devPop').show();
+          return $('html').addClass('noscroll');
+        });
+        $('body').on('click', '.devPopBg, .devPopModal span.close', function(e) {
+          e.preventDefault();
+          $popModal.animate({
+            top: -1000
+          }, 'fast', function() {
+            return $popBg.animate({
+              opacity: 0
+            }, 'slow', function() {
+              return $('#devPop').hide();
+            });
+          });
+          return $('html').removeClass('noscroll');
+        });
+        return log("\n========== END =========== \n ");
       });
     }
   });
+
+  _createModal = function() {
+    var $height, $width;
+    $('body').prepend('<div id="devPop" style="display:none"><div class="devPopModal paperstack"><span class="close">X</span></div><div class="devPopBg"/></div>');
+    log(' - modal created');
+    $height = $('.devPopModal').height();
+    log($width = ($(window).width() - $('.devPopModal').outerWidth()) / 2);
+    log($('.devPopModal').width());
+    log($('.devPopModal').outerWidth());
+    log($(window).width());
+    return $('.devPopModal').css({
+      top: -1 * ($height + 50),
+      left: $width
+    });
+  };
+
+  _setupDom = function(el) {
+    var $id, $pop, $popModal, $target;
+    $id = el.attr('href');
+    $target = $($id);
+    $pop = $('#devPop');
+    $popModal = $pop.find('.devPopModal');
+    log(' - hide content on init');
+    $target.css({
+      'display': 'none'
+    });
+    log(' - move content to modal');
+    $target.prependTo($popModal);
+    return log(' - dom set');
+  };
 
 }).call(this);
